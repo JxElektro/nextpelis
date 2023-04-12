63a74f6575931cad5dcff429c78770025cb9c9bb
Jhen Nunez <jxelektro@gmail.com>
2023-04-11 21:39:06 -0400

    visual changes

--- app/components/PopularMovies.tsx
+++ app/components/PopularMovies.tsx
@@ -1,77 +1,85 @@
 'use client'
 
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import React from 'react'
-import { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const PopularMovies = () => {
+  const [movies, setMovies] = useState<any[]>([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const API_TOKEN = "c2d1eba2da68e492d514141b781c25cf";
+
+  const fetchPopularMovies = async () => {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_TOKEN}&language=en-US&page=${page}`
+    );
+    const data = await res.json();
+    const results = Array.isArray(data.results) ? data.results : [];
 
 
-const PopularMovies = ({ title, id, poster_path, release_date } : {
-  title: string;  
-  id: number;
-  poster_path: string;
-  release_date: string;
-}
-  ) => {
- 
-  /*useEffect(() => {
-    console.log("PopularMovies");
-  }, []);*/
-  if (!poster_path) {
-    return null;
-  }
+    setMovies((prevMovies) => {
+      const uniqueMovies: { [key: number]: any } = {};
+      [...prevMovies, ...results].forEach((movie) => {
+        if (!uniqueMovies.hasOwnProperty(movie.id)) {
+          uniqueMovies[movie.id] = movie;
+        }
+      });
+      return Object.values(uniqueMovies);
+    });
 
-  const imageUrl = `https://image.tmdb.org/t/p/original${poster_path}`;
+    setTotalPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    fetchPopularMovies();
+  }, []);
+
+  const handleLoadMore = () => {
+    if (page < totalPages) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
 
   return (
-    <div className="flex flex-wrap justify-center items-center w-64 p-4">
-      
-      <div className="bg-gray rounded-lg shadow-lg overflow-hidden">
-        <Link href={`/${id}`} title={`${title}`}>
-          <Image src={imageUrl} alt={title} width={300} height={450} className="hover:opacity-75 transition ease-in-out duration-150" />
-        </Link>
-        <div className="p-4">
-          <h2 className="text-lg font-medium text-gray-900">{title}</h2>
-          <p className="text-sm font-medium text-gray-500">{release_date}</p>
-        </div>
-      </div>
-    
+    <div className="flex flex-wrap justify-center w-full">
+    <InfiniteScroll
+      dataLength={movies.length}
+      next={handleLoadMore}
+      hasMore={page < totalPages}
+      loader={<h4>Loading...</h4>}
+      className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+    >
+      {movies.map(({ id, title, poster_path, release_date }) => {
+        if (!poster_path) {
+          return null;
+        }
+
+        const imageUrl = `https://image.tmdb.org/t/p/original${poster_path}`;
+
+        return (
+          <div key={id} className="bg-gray rounded-lg shadow-lg overflow-hidden">
+            <Link href={`/${id}`} title={`${title}`}>
+              <Image
+                src={imageUrl}
+                alt={title}
+                width={300}
+                height={450}
+                className="hover:opacity-75 transition ease-in-out duration-150"
+              />
+            </Link>
+            <div className="p-4">
+              <h2 className="text-lg font-medium text-gray-900">{title}</h2>
+              <p className="text-sm font-medium text-gray-500">{release_date}</p>
+            </div>
+          </div>
+        );
+      })}
+    </InfiniteScroll>
     </div>
   );
-}
+};
 
-export default PopularMovies
-
-/*
-export default function PopularMovies({ title, id, poster_path, release_date }: {
-  title: string;
-  id: number;
-  poster_path: string;
-  release_date: string;
-}) {
-  if (!poster_path) {
-    return null;
-  }
-
-
-
- 
-
-  const imageUrl = `https://image.tmdb.org/t/p/original${poster_path}`;
-  return (
-    <div className="flex flex-wrap justify-center items-center w-64 p-4">
-      
-      <div className="bg-gray rounded-lg shadow-lg overflow-hidden">
-        <Link href={`/${id}`} title={`${title}`}>
-          <Image src={imageUrl} alt={title} width={300} height={450} className="hover:opacity-75 transition ease-in-out duration-150" />
-        </Link>
-        <div className="p-4">
-          <h2 className="text-lg font-medium text-gray-900">{title}</h2>
-          <p className="text-sm font-medium text-gray-500">{release_date}</p>
-        </div>
-      </div>
-    
-    </div>
-  );
-}
-*/
+export default PopularMovies;
--- app/components/PremieredMovies.tsx
+++ app/components/PremieredMovies.tsx
@@ -1,28 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function PremieredMovies({ title, id, backdrop_path, release_date }: {
+export default function PremieredMovies({ title, id, backdrop_path, release_date, poster_path }: {
   title: string;
   id: number;
-  backdrop_path: string;
+  backdrop_path: string | null;
   release_date: string;
+  poster_path: string ;
 }) {
-  if (!backdrop_path) {
-    return null;
-  }
-  const imageUrl = `https://image.tmdb.org/t/p/original${backdrop_path}`;
+
+  const imageUrl = `https://image.tmdb.org/t/p/original${backdrop_path || poster_path}`;
 
   return (
-    <div className="flex flex-col items-center p-4 md:p-2 gap-4 md:gap-0">
-      <div className="bg-gray rounded-lg shadow-lg overflow-hidden">
+    <div className="flex items-center max-w-fit pb-2 md:pb-4 gap-2 md:gap-4 ">
+      <div className="  overflow-hidden flex flex-col">
         <Link href={`/${id}`} title={`${title}`}>
-          <Image src={imageUrl} alt={title} width={936} height={120} className="hover:opacity-75 transition ease-in-out duration-150 md:w-468 md:h-60" />
+          <Image src={imageUrl} alt={title} width={936} height={120} className="hover:opacity-75 rounded-b-full transition ease-in-out duration-150 md:w-80 md:h-48" />
         </Link>
-        <div className="p-4 md:p-2">
-          <h2 className="text-lg font-medium text-gray-900">{title}</h2>
-          <p className="text-sm font-medium text-gray-500">{release_date}</p>
+        <div className="md:p-4 text-center align-middle">
+          <h2 className="text-xl font-medium text-gray-900 ">{title}</h2>
+          <p className="text-sm text-gray-600">{release_date}</p>
         </div>
       </div>
     </div>
   );
+
 }
